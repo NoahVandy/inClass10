e2a7c4654eb1d089b6f1de8e2d960ef8097a0505
Noah Vandervelden <NVandervel@my.gcu.edu>
2019-03-26 10:49:56 -0700

    The whole thing

--- inClass10/Program.cs
+++ inClass10/Program.cs
@@ -10,6 +10,29 @@ namespace inClass10
     {
         static void Main(string[] args)
         {
+            int count = 0;
+
+            string allText = System.IO.File.ReadAllText(@"C:\Users\noahv\OneDrive\Desktop\College\Computer Programming\C#\FilePractice.txt");
+
+
+            Console.WriteLine("Contents of FilePractice.txt = {0}", allText);
+
+
+            string[] words = allText.Split();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                
+
+                if (words[i].Substring(words[i].Length - 1) == "e" || words[i].Substring(words[i].Length - 1) == "t")
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("There are " + count + " words that end in e or t");
+
+
         }
     }
 }
